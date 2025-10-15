9872d559d2328f02ef39c74b13e510efacfea958
leonardou92 <urdaneta.leonardo92@gmail.com>
2025-10-15 16:53:19 -0400

    chore: enable permissive CORS (app.use(cors()))

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors()); // Permitir todos los orígenes
 
 // Human-friendly colorized console logger
 app.use((req, res, next) => {
